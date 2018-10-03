8f431bdef590033fcd417e5517eb638dc6d604d6
tommygod3 <tommygod1@live.com>
2018-10-03 09:21:30 +0100

    Moved data and scripts into different directories, completed exercises9-10

---
+++ scripts/exercise10.py
@@ -0,0 +1,31 @@
+print("---------- Exercise 10 script ----------")
+
+print("Question 1:")
+a = set(range(6))
+b = set(range(2,9,2))
+print(f"a: {a}")
+print(f"b: {b}")
+print(f"a union b: {a.union(b)}")
+print(f"a intersect b: {a.intersection(b)}")
+
+print("Question 2:")
+band = ["mel", "geri", "victoria", "mel", "emma"]
+counts = {}
+for member in band:
+    if member in counts.keys():
+        counts[member] += 1
+    else:
+        counts[member] = 1
+for count in counts:
+    print(f"Member: {count}, count: {counts[count]}")
+
+print("Question 3:")
+if {}: print("hi") # Will not print
+d = {"maggie": "uk", "ronnie": "usa"}
+print(f"dir(d):\n{dir(d)}")
+print(d.items())
+print(d.keys())
+print(d.values())
+print(f"maggie: {d.get('maggie','Not found!')}") # If key not found, will print 'Not found!'
+print(f"mikhail: {d.get('mikhail','Not found!')}")
+print(f"mikhail: {d.setdefault('mikhail','ussr')}") # If mikhail not found, will add to dict with value ussr
